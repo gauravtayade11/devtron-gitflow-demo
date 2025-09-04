d8359866811a65a1eff777353fcd930a0c162e2d
Gaurav Tayade <gauravtayade1192@gmail.com>
2025-09-04 12:35:15 +0530

    added services feature

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ app.get("/", (req, res) => res.send("Hello from Devtron + Git Flow 🚀"));
 
 app.get("/about", (req, res) => res.send("This is Hello World App - About Page!"));
 
+app.get('/services', (req, res) => {
+  res.send('Our Services');
+});
+
 app.listen(port, () => console.log(`App running at http://localhost:${port}`));
